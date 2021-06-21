3e3d98b75d36081a7e350f92390255ef9ebbf076
PRIYANSHU DAS <ask.priyanshu@gmail.com>
2021-06-22 02:09:11 +0530

    Done the Login Page UI(Have to add controllers for input)

--- app/lib/pages/LoginUI.dart
+++ app/lib/pages/LoginUI.dart
@@ -94,31 +94,199 @@ class Login extends StatelessWidget {
 class EmailRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              indicator: BoxDecoration(
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          bottom: PreferredSize(
+            preferredSize: Size(MediaQuery.of(context).size.width / 1.5, 50),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 1.5,
+              child: TabBar(
+                unselectedLabelColor: Color(0xFF998FA2),
+                indicatorPadding: EdgeInsets.symmetric(horizontal: 15),
+                indicator: BoxDecoration(
                   borderRadius: BorderRadius.circular(50), // Creates border
-                  color: Color(0xFF8A56AC)),
-              tabs: [
-                Text("SIGN IN"),
-                Text("SIGN UP"),
-              ],
+                  color: Color(0xFF8A56AC),
+                ),
+                tabs: <Widget>[
+                  Container(
+                    height: 50,
+                    decoration: BoxDecoration(),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("SIGN IN"),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    decoration: BoxDecoration(),
+                    padding: EdgeInsets.all(10),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("SIGN UP"),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          body: TabBarView(
-            children: [
-              Container(),
-              Container(),
-            ],
-          ),
         ),
-      )),
+        body: TabBarView(
+          children: [
+            SignIn(),
+            SignUp(),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class SignIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Padding(
+            padding: EdgeInsets.only(top: 50, bottom: 40, left: 25, right: 25),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF000000)),
+                      ),
+                      hintText: 'Email',
+                      hintStyle:
+                          TextStyle(fontSize: 16.0, color: Color(0x77241332)),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF000000)),
+                      ),
+                      hintText: 'Password',
+                      hintStyle:
+                          TextStyle(fontSize: 16.0, color: Color(0x77241332)),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.only(left: 5, right: 5, top: 60, bottom: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Color(0xff8A56AC)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ))),
+                    child: Padding(
+                        padding: EdgeInsets.only(top: 20, bottom: 20),
+                        child: Text('CONTINUE')),
+                  ),
+                ),
+                Container(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xff8A56AC)),
+                    ),
+                    onPressed: () {},
+                    child: Text('FORGOT PASSWORD'),
+                  ),
+                )
+              ],
+            )));
+  }
+}
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Padding(
+            padding: EdgeInsets.only(top: 50, bottom: 40, left: 25, right: 25),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF000000)),
+                      ),
+                      hintText: 'Email',
+                      hintStyle:
+                          TextStyle(fontSize: 16.0, color: Color(0x77241332)),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF000000)),
+                      ),
+                      hintText: 'Password',
+                      hintStyle:
+                          TextStyle(fontSize: 16.0, color: Color(0x77241332)),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.only(left: 5, right: 5, top: 60, bottom: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Color(0xff8A56AC)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ))),
+                    child: Padding(
+                        padding: EdgeInsets.only(top: 20, bottom: 20),
+                        child: Text('CONTINUE')),
+                  ),
+                ),
+                Container(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xff8A56AC)),
+                    ),
+                    onPressed: () {},
+                    child: Text('FORGOT PASSWORD'),
+                  ),
+                )
+              ],
+            )));
   }
 }
 
